f6e1b462c23955d1f29432f3727358ac8507c57b
wang.yi <wang.yi@qunar.com>
2013-12-01 14:50:21 +0800

    change workon_home

--- env/python.sh
+++ env/python.sh
@@ -1,13 +1,17 @@
 host=$(hostname)
+WORKON_HOME=~/.virtualenvs
 case $host in
     darwin*)
         PY_HOME=/usr/local/bin;;
     arch*)
         PY_HOME=/usr/bin;;
     l-*)
-        PY_HOME=/home/q/python/default/bin;;
+        PY_HOME=/home/q/python/default/bin
+        WORKON_HOME=/home/q/python/env
+        ;;
     *)
-        PY_HOME=/usr/bin
+        PY_HOME=/usr/bin;;
 esac
 
 export PY_HOME
+export WORKON_HOME
