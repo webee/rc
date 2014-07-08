e0cf67a9583df856c0678dea517c9227703911f1
wang.yi <wang.yi@qunar.com>
2014-07-08 20:37:14 +0800

    update.

--- env/python.corp.sh
+++ env/python.corp.sh
@@ -1,3 +1,17 @@
+function pathmunge {
+    if echo $PATH | egrep -q "(^|:)$1($|:)";then
+        a=${1//\//\\\/}
+        PATH=$(echo $PATH|sed -e "s/\([:^]*\)$a\([$:]*\)/\1\2/g")
+        PATH=$(echo $PATH|sed -e 's/::/:/g'|sed -e 's/^://g'|sed -e 's/:$//g')
+    fi
+    if [ "$2" = "after" ];then
+        PATH=$PATH:$1
+    else
+        PATH=$1:$PATH
+    fi
+    export PATH
+}
+
 PY_HOME=/home/q/python/default/bin
 WORKON_HOME=/home/q/python/env
 
@@ -5,3 +19,9 @@ export PY_HOME
 export WORKON_HOME
 
 export VIRTUALENVWRAPPER_PYTHON=${PY_HOME}/python
+
+export PYENV_ROOT="/home/q/python/pyenv"
+pathmunge "$PYENV_ROOT/bin"
+eval "$(pyenv init -)"
+
+export PYENV_VIRTUALENVWRAPPER_PREFER_PYVENV="true"
--- env/python_after.sh
+++ env/python_after.sh
@@ -4,6 +4,7 @@ set -e
 
 root=$(dirname $0)
 
+source /etc/profile
 source /etc/profile.d/python.sh
 if ! cp $root/pyenvrun.sh ${PY_HOME}/pyenvrun >/dev/null 2>&1; then
     sudo cp $root/pyenvrun.sh ${PY_HOME}/pyenvrun
