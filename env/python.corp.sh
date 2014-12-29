b80c2fcdca1c13511888a8612056c76e36c511ee
wang.yi <wang.yi@qunar.com>
2014-12-29 12:54:29 +0800

    update.

--- env/python.corp.sh
+++ env/python.corp.sh
@@ -13,10 +13,10 @@ function pathmunge {
 }
 
 PY_HOME=/home/q/python/default/bin
-WORKON_HOME=/home/q/python/env
+#WORKON_HOME=/home/q/python/env
 
 export PY_HOME
-export WORKON_HOME
+#export WORKON_HOME
 
 export VIRTUALENVWRAPPER_PYTHON=${PY_HOME}/python
 
